0868dd0d8b089b99e7151f479a6d4ba62144af1f
zg.qiu <zg.qiu@alcatel-lucent.com>
2016-05-06 19:55:36 +0800

    create domain/zone/subnet ok

--- nfv-test.py
+++ nfv-test.py
@@ -1,18 +1,138 @@
-import sys
+# Copyright 2014 Alcatel-Lucent USA Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""The module implements the Nuage Openstack NFV performance test."""
+import argparse
+import getpass
 import logging
-from vspk.v3_2 import *
+import random
+import string
+
+try:
+    from vspk import v3_2 as vsdk
+except ImportError:
+    from vspk.vsdk import v3_2 as vsdk
+
+RANDOM_NAME = ""
 
 
-session = NUVSDSession(username=u'csproot', password=u'csproot', enterprise=u'csp', api_url=u'https://135.227.154.21:8443')
-session.start()
-csproot = session.user
+def get_args():
+    """Support the command-line arguments listed below."""
+    parser = argparse.ArgumentParser(description="Tool to do performace test for Openstack Nuage NFV.")
+    parser.add_argument('-d', '--debug', required=False, help='Enable debug output', dest='debug', action='store_true')
+    parser.add_argument('-l', '--log-file', required=False, help='File to log to (default = stdout)', dest='logfile', type=str)
+    parser.add_argument('-E', '--nuage-organization', required=True, help='The organization with which to connect to the Nuage VSD/SDK host', dest='nuage_organization', type=str)
+    parser.add_argument('-H', '--nuage-host', required=True, help='The Nuage VSD/SDK endpoint to connect to', dest='nuage_host', type=str)
+    parser.add_argument('-P', '--nuage-port', required=False, help='The Nuage VSD/SDK server port to connect to (default = 8443)', dest='nuage_port', type=int, default=8443)
+    parser.add_argument('-p', '--nuage-password', required=False, help='The password with which to connect to the Nuage VSD/SDK host. If not specified, the user is prompted at runtime for a password', dest='nuage_password', type=str)
+    parser.add_argument('-u', '--nuage-user', required=True, help='The username with which to connect to the Nuage VSD/SDK host', dest='nuage_username', type=str)
+    parser.add_argument('-v', '--verbose', required=False, help='Enable verbose output', dest='verbose', action='store_true')
+    args = parser.parse_args()
+    return args
 
-# Count will make a request to the backend to retrieve the number of enterprises
-(_, _, nb_enterprises) = csproot.enterprises.count()
-print 'Number of enterprises to retrieve = %s' % nb_enterprises
 
-# Fetch will get all information of each enterprise from the server
-csproot.enterprises.fetch()
+def worker(logger, session):
+    """Prepare enterprise, domain and subnets etc on VSD."""
+    global RANDOM_NAME
+    enterprise_name = u"NFV-ENTERPRISE-" + RANDOM_NAME
+    domain_name = u"NFV-DOMAIN-" + RANDOM_NAME
+    zone_name = u"NFV-ZONE-" + RANDOM_NAME
+    subnet_name = u"NFV-SUBNET-" + RANDOM_NAME
+    vport_name = u"NFV-VM-" + RANDOM_NAME
 
-for enterprise in csproot.enterprises:
-    print enterprise.name
+    try:
+        # Create an enterprise with random name
+        logger.info('Creating enterprise %s on VSD.' % (enterprise_name))
+        enterprise = vsdk.NUEnterprise(name=enterprise_name)
+        session.create_child(enterprise, async=False)
+
+        logger.info('Create domain %s on VSD.' % domain_name)
+        domain_template = vsdk.NUDomainTemplate(name=domain_name)
+        enterprise.create_child(domain_template, async=False)
+        domain = vsdk.NUDomain(name=domain_name, template_id=domain_template.id)
+        enterprise.create_child(domain, async=False)
+
+        logger.info('Associate floating ip to the domain %s on VSD.' % domain_name)
+
+        logger.info('Create zone %s in the domain %s on VSD.' % (zone_name, domain_name))
+        zone = vsdk.NUZone(name=zone_name, async=False)
+        domain.create_child(zone, async=False)
+
+        for i in range(2):
+            address = "10.10." + str(i) + ".0"
+            gateway = "10.10." + str(i) + ".1"
+            netmask = "255.255.255.0"
+            logger.info('Create subnet %s in the zone %s on VSD.' % (subnet_name + str(i), zone_name))
+            subnet = vsdk.NUSubnet(name=subnet_name + str(i), gateway=gateway, address=address, netmask=netmask)
+            zone.create_child(subnet, async=False)
+    except Exception, e:
+        logger.error('NFV Testing on VSD failed.')
+        logger.critical('Caught exception: %s' % str(e))
+        return 1
+
+    return 0
+
+
+def main():
+    """Main function to handle NFV performance test."""
+    # Handling arguments
+    args = get_args()
+    debug = args.debug
+    log_file = None
+    if args.logfile:
+        log_file = args.logfile
+    nuage_organization = args.nuage_organization
+    nuage_host = args.nuage_host
+    nuage_port = args.nuage_port
+    nuage_password = None
+    if args.nuage_password:
+        nuage_password = args.nuage_password
+    nuage_username = args.nuage_username
+    verbose = args.verbose
+
+    global RANDOM_NAME
+    RANDOM_NAME = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    # Logging settings
+    if debug:
+        log_level = logging.DEBUG
+    elif verbose:
+        log_level = logging.INFO
+    else:
+        log_level = logging.WARNING
+
+    logging.basicConfig(filename=log_file, format='%(asctime)s %(filename)s %(levelname)s %(message)s', level=log_level)
+    logger = logging.getLogger(__name__)
+
+    # Getting user password for Nuage connection
+    if nuage_password is None:
+        logger.debug('No command line Nuage password received, requesting Nuage password from user')
+        nuage_password = getpass.getpass(prompt='Enter password for Nuage host %s for user %s: ' % (nuage_host, nuage_username))
+
+    try:
+        # Connecting to Nuage
+        logger.info('Connecting to Nuage server %s:%s with username %s' % (nuage_host, nuage_port, nuage_username))
+        nc = vsdk.NUVSDSession(username=nuage_username, password=nuage_password, enterprise=nuage_organization, api_url="https://%s:%s" % (nuage_host, nuage_port))
+        nc.start()
+        session = nc.user
+    except Exception, e:
+        logger.error('Could not connect to Nuage host %s with user %s and specified password' % (nuage_host, nuage_username))
+        logger.critical('Caught exception: %s' % str(e))
+        return 1
+
+    worker(logger, session)
+    return 0
+
+# Start program
+if __name__ == "__main__":
+    main()
